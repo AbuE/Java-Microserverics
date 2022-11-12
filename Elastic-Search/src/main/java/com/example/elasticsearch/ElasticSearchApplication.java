77ca1f2674229ee08b0426320702d668faf65278
DESKTOP-G372D1G\user <abu.ehtesham@gmail.com>
2022-11-12 21:34:52 +0530

    Removed Discover Client - Converted to independent spring boot

--- Elastic-Search/src/main/java/com/example/elasticsearch/ElasticSearchApplication.java
+++ Elastic-Search/src/main/java/com/example/elasticsearch/ElasticSearchApplication.java
@@ -2,12 +2,10 @@ package com.example.elasticsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableElasticsearchRepositories(basePackages = "com.example.repository")
 @ComponentScan(basePackages  = "com.example.controller")
 public class ElasticSearchApplication {
